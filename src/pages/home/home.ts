3c2d3ff0b29572ad83cd016b1280e3f63b8bbc02
lucas-rafa-94 <lucas_rafa_94@hotmail.com>
2016-10-26 04:27:38 -0200

    C

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,6 +7,9 @@ import { Geolocation } from 'ionic-native';
 
 declare var google;
 
+var longitude: number = 0;
+var latitude: number = 0;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -16,8 +19,10 @@ export class HomePage {
   @ViewChild('map') mapElement: ElementRef;
   map: any;
 
-  constructor(public navCtrl: NavController) {
 
+
+  constructor(public navCtrl: NavController) {
+    this.ionViewLoaded();
   }
   addInfoWindow(marker, content) {
 
@@ -33,6 +38,9 @@ export class HomePage {
   ionViewLoaded() {
     this.loadMap();
   }
+  ionViewLoadedBusca() {
+    this.loadMapBusca();
+  }
   addMarker() {
 
     let marker = new google.maps.Marker({
@@ -41,18 +49,38 @@ export class HomePage {
       position: this.map.getCenter()
     });
 
-    let content = "<h4>Information!</h4>";
-
+    let content = "<h4>People4People</h4>";
+    console.log(this.map.getCenter());
     this.addInfoWindow(marker, content);
 
   }
 
+  buscaEnd(endereco) {
+
+    var geocoder = new google.maps.Geocoder();
+    console.log(endereco)
+
+    geocoder.geocode({ 'address': endereco }, function(results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        console.log("++++++++++++++++++++");
+        console.log(results);
+        latitude = results[0].geometry.bounds.b.b;
+
+        longitude = results[0].geometry.bounds.f.b;
+
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+    this.ionViewLoadedBusca();
+
+  }
   loadMap() {
 
     Geolocation.getCurrentPosition().then((position) => {
-
+      console.log(position);
       let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
+      console.log(latLng);
       let mapOptions = {
         center: latLng,
         zoom: 15,
@@ -65,6 +93,20 @@ export class HomePage {
     }, (err) => {
       console.log(err);
     });
+
+  }
+  loadMapBusca() {
+
+    console.log(longitude + "  " + latitude);
+    let latLng = new google.maps.LatLng(longitude, latitude);
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    console.log(mapOptions);
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
 
   }
 }
